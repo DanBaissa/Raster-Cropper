2b7029f27e8beb321b986e45dd488dafcf380ca2
danbaissa <danbaissa@gmail.com>
2023-07-06 19:21:46 -0400

    prepping for standalone app

---
+++ hook-rasterio.py
@@ -0,0 +1,13 @@
+hiddenimports = [
+    'rasterio._base',
+    'rasterio._io',
+    'rasterio._shim',
+    'rasterio._env',
+    'rasterio._pyproj',
+    'rasterio._features',
+    'rasterio._fill',
+    'rasterio._err',
+    'rasterio.transform',
+    'rasterio.warp',
+    'rasterio.sample',
+]
