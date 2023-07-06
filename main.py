8c0ff85160e5cfbefec72b183976ab36fb6b0526
danbaissa <danbaissa@gmail.com>
2023-07-06 17:22:51 -0400

    preping for standalone app

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import glob
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter import ttk
@@ -46,6 +47,9 @@ def crop_raster_with_shapefile(raster_path, country_shape, output_path):
         "transform": out_transform
     })
 
+    # Create directory for output file if it doesn't exist
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
     # Write the cropped raster to a new file
     print('Writing cropped raster to new file...')
     with rasterio.open(output_path, "w", **out_meta) as dest:
@@ -57,38 +61,67 @@ def crop_raster_with_shapefile(raster_path, country_shape, output_path):
 
     print('Finished cropping raster file.')
 
-def select_raster_file():
-    file_path = filedialog.askopenfilename()
-    raster_file_var.set(file_path)
+def select_raster_dir():
+    dir_path = filedialog.askdirectory()
+    raster_dir_var.set(dir_path)
 
-def select_output_file():
-    file_path = filedialog.asksaveasfilename(defaultextension=".tif")
-    output_file_var.set(file_path)
+def select_output_dir():
+    dir_path = filedialog.askdirectory()
+    output_dir_var.set(dir_path)
+
 
 def run():
     try:
-        print('Running...')
         country = shapefile[shapefile['COUNTRY'] == country_var.get()]
-        crop_raster_with_shapefile(raster_file_var.get(), country, output_file_var.get())
-        print('Done.')
+        raster_files = glob.glob(os.path.join(raster_dir_var.get(), '*.tif'))
+
+        for raster_file in raster_files:
+            output_dir = os.path.normpath(output_dir_var.get()).replace("\\", "/")
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+
+            raster_file = os.path.normpath(raster_file).replace("\\", "/")
+            output_file = os.path.join(output_dir, os.path.basename(raster_file)).replace("\\", "/")
+            print('Raster file:', raster_file)
+            print('Output file:', output_file)
+            crop_raster_with_shapefile(raster_file, country, output_file)
+
+            with rasterio.open(output_file) as src:
+                img = src.read(1)
+
+            # Plotting
+            fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 6))
+
+            # Standard plot
+            ax1.imshow(img, cmap='turbo')
+            ax1.set_title("Standard")
+
+            # Log transformed plot
+            # Adding small constant to avoid log(0)
+            ax2.imshow(np.log1p(img), cmap='turbo')
+            ax2.set_title("Log transformed")
+
+            plt.show()
+
         root.update()
     except Exception as e:
+        print('Error:', str(e))
         messagebox.showerror("Error", str(e))
 
 root = tk.Tk()
 
-raster_file_var = tk.StringVar()
-output_file_var = tk.StringVar()
+raster_dir_var = tk.StringVar()
+output_dir_var = tk.StringVar()
 country_var = tk.StringVar()
 
 # Get sorted unique country names
 country_options = sorted(shapefile['COUNTRY'].unique())
 
-raster_file_button = tk.Button(root, text="Select Raster File", command=select_raster_file)
-raster_file_button.pack()
+raster_dir_button = tk.Button(root, text="Select Raster Directory", command=select_raster_dir)
+raster_dir_button.pack()
 
-output_file_button = tk.Button(root, text="Select Output File", command=select_output_file)
-output_file_button.pack()
+output_dir_button = tk.Button(root, text="Select Output Directory", command=select_output_dir)
+output_dir_button.pack()
 
 country_dropdown = ttk.Combobox(root, textvariable=country_var)
 country_dropdown['values'] = country_options
